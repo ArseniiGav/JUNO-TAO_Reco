f5a3878ccbcc3b44d32a58c5379e43f08e357a9f
ArseniiGav <gavrikov.997755@gmail.com>
2022-08-21 21:43:06 +0300

    preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,72 @@
+import uproot3
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+
+bl = int(sys.argv[1])
+br = int(sys.argv[2])
+test = sys.argv[3]
+en = sys.argv[4]
+
+for j in range(bl, br):
+    root_file = f"sample_elecsim-{j}.root"
+    if test:
+        eos_path = f"/eos/juno/users/a/arsg/TAO_test/{en}MeV"
+    else:
+        eos_path = "/eos/juno/users/a/arsg/TAO/"
+
+    os.system("export EOS_MGM_URL=root://eos.jinr.ru")
+    os.system(f"xrdcp root://eos.jinr.ru/{eos_path}/{root_file} TAO_data/{root_file}")
+
+    file = uproot3.open(f"TAO_data/{root_file}")
+    df = file["Event/Sim/SimEvent"].pandas.df(flatten=False)
+
+    edeps = []
+    edepXs = []
+    edepYs = []
+    edepZs = []
+
+    HitTs = []
+    HitIds = []
+
+    for evt in range(len(df)):
+        edeps.append(df.iloc[evt].fGdLSEdep)
+        edepXs.append(df.iloc[evt].fGdLSEdepX / 1000.)
+        edepYs.append(df.iloc[evt].fGdLSEdepY / 1000.)
+        edepZs.append(df.iloc[evt].fGdLSEdepZ / 1000.)  
+
+        HitT = np.array(df.iloc[evt].fSiPMHitT)
+        HitId = np.array(df.iloc[evt].fSiPMHitID)
+
+        HitTs.append(HitT)
+        HitIds.append(HitId)
+
+    HitTs = np.array(HitTs, dtype=object)
+    HitIds = np.array(HitIds, dtype=object)
+    raw_data = np.vstack((HitIds, HitTs))
+
+    data = np.vstack(
+        (edeps, edepXs, edepYs, edepZs)
+    ).T
+
+    description = "Data description: \n \
+    By 'hits' key: SiPMHitID,  SiPMHitT \n \
+    By 'primaries' key: edep, edepX, edepY, edepZ"
+    if test:
+        np.savez_compressed(
+            f"/mnt/cephfs/ml_data/TAO_detsim_J22/test/{en}MeV/detsim_" + root_file.split("-")[1][:-5] + ".npz",
+            hits=raw_data,
+            primaries=data,
+            description=description
+        )
+    else:
+        np.savez_compressed(
+            "/mnt/cephfs/ml_data/TAO_detsim_J22/train/detsim_" + root_file.split("-")[1][:-5] + ".npz",
+            hits=raw_data,
+            primaries=data,
+            description=description
+        )
+
+    os.system(f"rm TAO_data/{root_file}")
--- run_preprocessing.py
+++ run_preprocessing.py
@@ -1,12 +1,17 @@
 import os
 import sys
 
-step = int(sys.argv[1])
-shift = int(sys.argv[2])
+shift = int(sys.argv[1])
+step = int(sys.argv[2])
 N = int(sys.argv[3])
+mode = sys.argv[4]
 
 for i in range(N):
-    command = f"python preprocessing_train.py {i*step+shift} {(i+1)*step+shift} &"
+    if mode == "train":
+        command = f"python preprocessing.py {i*step+shift} {(i+1)*step+shift} False &"
+    else:
+        energy = sys.argv[5]
+        command = f"python preprocessing.py {i*step+shift} {(i+1)*step+shift} True {energy} &"
     os.system(command)
     print(command)
 
